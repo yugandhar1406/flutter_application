c80e766131f0c6c6a7713283d37c9248354a4dec
yugandhar1406 <101722350+yugandhar1406@users.noreply.github.com>
2026-03-03 17:20:35 +0530

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'home_page.dart';
+import 'stats_page.dart';
+import 'rewards_page.dart';
+import 'goal_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    HomePage(),
+    StatsPage(),
+    RewardsPage(),
+    GoalPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: pages[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.blue,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart),
+              label: "Stats",
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "Rewards"),
+            BottomNavigationBarItem(icon: Icon(Icons.flag), label: "Goal"),
+          ],
+        ),
+      ),
+    );
   }
 }
