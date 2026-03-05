a1a5f2cbc7e88a210d906dc1edf13b5d79a20a03
yugandhar1406 <101722350+yugandhar1406@users.noreply.github.com>
2026-03-05 16:17:55 +0530

    Create Whatsapp page with basic UI structure

---
+++ lib/Whatsapp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Whatsapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Rewards")),
+      body: Center(
+        child: Text("This is whatasapp Page", style: TextStyle(fontSize: 22)),
+      ),
+    );
+  }
+}
