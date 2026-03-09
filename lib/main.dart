084df6afdc1afc8602b747d36e015534448ce1cc
yugandhar1406 <101722350+yugandhar1406@users.noreply.github.com>
2026-03-09 12:13:45 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,37 +17,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ResponsivePage extends StatelessWidget {
-  ResponsivePage({super.key});
+class ResponsivePage extends StatefulWidget {
+  const ResponsivePage({super.key});
 
-  final List<String> items =
-      List.generate(10, (index) => "Item ${index + 1}");
+  @override
+  State<ResponsivePage> createState() => _ResponsivePageState();
+}
+
+class _ResponsivePageState extends State<ResponsivePage> {
+  List<String> items = List.generate(10, (index) => "Item ${index + 1}");
 
   @override
   Widget build(BuildContext context) {
-
     double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Screen Width: $width"),
-      ),
+      appBar: AppBar(title: Text("Screen Width: $width")),
 
       body: width < 600
-      
           ? ListView.builder(
               itemCount: items.length,
               itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    leading: Icon(Icons.star),
-                    title: Text(items[index]),
+                return Dismissible(
+                  key: Key(items[index]),
+
+                  onDismissed: (direction) {
+                    setState(() {
+                      items.removeAt(index);
+                    });
+                  },
+
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 20),
+                    child: Icon(Icons.delete, color: Colors.white),
+                  ),
+
+                  child: Card(
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(title: Text(items[index])),
                   ),
                 );
               },
             )
-
           : GridView.builder(
               padding: EdgeInsets.all(10),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -57,12 +70,24 @@ class ResponsivePage extends StatelessWidget {
               ),
               itemCount: items.length,
               itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  child: Center(
-                    child: Text(
-                      items[index],
-                      style: TextStyle(fontSize: 18),
+                return Dismissible(
+                  key: Key(items[index]),
+
+                  onDismissed: (direction) {
+                    setState(() {
+                      items.removeAt(index);
+                    });
+                  },
+
+                  background: Container(
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Icon(Icons.delete, color: Colors.white),
+                  ),
+
+                  child: Card(
+                    child: Center(
+                      child: Text(items[index], style: TextStyle(fontSize: 18)),
                     ),
                   ),
                 );
